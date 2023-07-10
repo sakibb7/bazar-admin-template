52f8405083e1e052261a88253484f1fe5f34fc46
Mohammad Sakib <sakibb076@gmail.com>
2023-07-10 12:35:33 +0600

    add sorting functionality in product section

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,72 +1,624 @@
-export const products = [
-    {
-        id:1,
-        title:`A Walk Amongst Friends - Canvas Print`,
-        category:`Nature`,
-        price:25.6,
-        quantity:64,
-    },
-    {
-        id:2,
-        title:`Braies Lake - Canvas Print`,
-        category:`Paint`,
-        price:68.6,
-        quantity:0,
-    },
-    {
-        id:3,
-        title:`Fall Glow - Canvas Print`,
-        category:`Canvas`,
-        price:45.5,
-        quantity:25,
-    },
-    {
-        id:4,
-        title:`Lago di Braies - Canvas Print`,
-        category:`Nature`,
-        price:47,
-        quantity:65,
-    },
-    {
-        id:5,
-        title:`Never Stop Changing - Canvas Print`,
-        category:`Paint`,
-        price:15,
-        quantity:0,
-    },
-    {
-        id:6,
-        title:`Reaching - Canvas Print`,
-        category:`Canvas`,
-        price:98.2,
-        quantity:14,
-    },
-    {
-        id:7,
-        title:`Yosemite - Canvas Print`,
-        category:`Nature`,
-        price:75.8,
-        quantity:9,
-    },
-    {
-        id:8,
-        title:`Morain Lake - Canvas Print`,
-        category:`Print`,
-        price:64.5,
-        quantity:0,
-    },
-    {
-        id:9,
-        title:`Lago di Sorapis - Canvas Print`,
-        category:`Print`,
-        price:25.6,
-        quantity:8,
-    },
-    {
-        id:10,
-        title:`First Snow - Canvas Print`,
-        category:`Canvas`,
-        price:17.8,
-        quantity:4,
-    },
+export const tableHead = [
+  {
+    id:1,
+    name:"No",
+  },
+  {
+    id:2,
+    name:"Name",
+  },
+  {
+    id:3,
+    name:"Category",
+  },
+  {
+    id:4,
+    name:"Price",
+  },
+  {
+    id:5,
+    name:"Quantify",
+  },
 ]
+
+export const products = [{
+    "id": 1,
+    "title": "Lamb - Loin Chops",
+    "price": 16.44,
+    "quantity": 74,
+    "category": "personal care"
+  }, {
+    "id": 2,
+    "title": "Tea - English Breakfast",
+    "price": 54.25,
+    "quantity": 0,
+    "category": "food"
+  }, {
+    "id": 3,
+    "title": "Bread - White, Sliced",
+    "price": 4.63,
+    "quantity": 20,
+    "category": "personal care"
+  }, {
+    "id": 4,
+    "title": "Wine - Chablis 2003 Champs",
+    "price": 97.45,
+    "quantity": 0,
+    "category": "household"
+  }, {
+    "id": 5,
+    "title": "Tea - Lemon Green Tea",
+    "price": 82.05,
+    "quantity": 52,
+    "category": "food"
+  }, {
+    "id": 6,
+    "title": "Tofu - Firm",
+    "price": 28.68,
+    "quantity": 2,
+    "category": "food"
+  }, {
+    "id": 7,
+    "title": "Apple - Fuji",
+    "price": 84.83,
+    "quantity": 86,
+    "category": "beverages"
+  }, {
+    "id": 8,
+    "title": "Oven Mitt - 13 Inch",
+    "price": 82.52,
+    "quantity": 24,
+    "category": "personal care"
+  }, {
+    "id": 9,
+    "title": "Coffee - Egg Nog Capuccino",
+    "price": 26.87,
+    "quantity": 97,
+    "category": "household"
+  }, {
+    "id": 10,
+    "title": "Pasta - Fettuccine, Egg, Fresh",
+    "price": 90.97,
+    "quantity": 0,
+    "category": "food"
+  }, {
+    "id": 11,
+    "title": "Yogurt - Plain",
+    "price": 51.66,
+    "quantity": 55,
+    "category": "beverages"
+  }, {
+    "id": 12,
+    "title": "Veal - Loin",
+    "price": 10.22,
+    "quantity": 100,
+    "category": "personal care"
+  }, {
+    "id": 13,
+    "title": "Papadam",
+    "price": 70.01,
+    "quantity": 63,
+    "category": "personal care"
+  }, {
+    "id": 14,
+    "title": "Sauce Bbq Smokey",
+    "price": 13.29,
+    "quantity": 17,
+    "category": "personal care"
+  }, {
+    "id": 15,
+    "title": "Pepper - Chili Powder",
+    "price": 37.92,
+    "quantity": 70,
+    "category": "food"
+  }, {
+    "id": 16,
+    "title": "Napkin White - Starched",
+    "price": 91.51,
+    "quantity": 70,
+    "category": "personal care"
+  }, {
+    "id": 17,
+    "title": "Vodka - Moskovskaya",
+    "price": 1.21,
+    "quantity": 10,
+    "category": "household"
+  }, {
+    "id": 18,
+    "title": "Jack Daniels",
+    "price": 93.64,
+    "quantity": 0,
+    "category": "food"
+  }, {
+    "id": 19,
+    "title": "Butter - Salted",
+    "price": 98.66,
+    "quantity": 32,
+    "category": "household"
+  }, {
+    "id": 20,
+    "title": "Cleaner - Pine Sol",
+    "price": 27.94,
+    "quantity": 50,
+    "category": "personal care"
+  }, {
+    "id": 21,
+    "title": "Sherry - Dry",
+    "price": 55.16,
+    "quantity": 63,
+    "category": "household"
+  }, {
+    "id": 22,
+    "title": "Browning Caramel Glace",
+    "price": 15.28,
+    "quantity": 47,
+    "category": "personal care"
+  }, {
+    "id": 23,
+    "title": "Nestea - Iced Tea",
+    "price": 43.99,
+    "quantity": 71,
+    "category": "personal care"
+  }, {
+    "id": 24,
+    "title": "Triple Sec - Mcguinness",
+    "price": 49.0,
+    "quantity": 54,
+    "category": "food"
+  }, {
+    "id": 25,
+    "title": "Wine - Rosso Toscano Igt",
+    "price": 60.32,
+    "quantity": 42,
+    "category": "beverages"
+  }, {
+    "id": 26,
+    "title": "Sauce - Salsa",
+    "price": 74.08,
+    "quantity": 23,
+    "category": "beverages"
+  }, {
+    "id": 27,
+    "title": "Cherries - Maraschino,jar",
+    "price": 23.66,
+    "quantity": 57,
+    "category": "beverages"
+  }, {
+    "id": 28,
+    "title": "Stock - Chicken, White",
+    "price": 24.46,
+    "quantity": 42,
+    "category": "personal care"
+  }, {
+    "id": 29,
+    "title": "Bread - Raisin",
+    "price": 94.65,
+    "quantity": 38,
+    "category": "beverages"
+  }, {
+    "id": 30,
+    "title": "Honey - Comb",
+    "price": 57.16,
+    "quantity": 59,
+    "category": "food"
+  }, {
+    "id": 31,
+    "title": "Shallots",
+    "price": 68.12,
+    "quantity": 9,
+    "category": "personal care"
+  }, {
+    "id": 32,
+    "title": "Lemonade - Island Tea, 591 Ml",
+    "price": 73.34,
+    "quantity": 2,
+    "category": "food"
+  }, {
+    "id": 33,
+    "title": "Island Oasis - Lemonade",
+    "price": 26.65,
+    "quantity": 43,
+    "category": "household"
+  }, {
+    "id": 34,
+    "title": "Gherkin",
+    "price": 78.38,
+    "quantity": 10,
+    "category": "household"
+  }, {
+    "id": 35,
+    "title": "Sesame Seed",
+    "price": 2.92,
+    "quantity": 60,
+    "category": "beverages"
+  }, {
+    "id": 36,
+    "title": "Yokaline",
+    "price": 12.39,
+    "quantity": 6,
+    "category": "food"
+  }, {
+    "id": 37,
+    "title": "Gelatine Leaves - Bulk",
+    "price": 43.4,
+    "quantity": 8,
+    "category": "household"
+  }, {
+    "id": 38,
+    "title": "Cheese - Cambozola",
+    "price": 28.34,
+    "quantity": 53,
+    "category": "beverages"
+  }, {
+    "id": 39,
+    "title": "Crush - Orange, 355ml",
+    "price": 68.14,
+    "quantity": 41,
+    "category": "personal care"
+  }, {
+    "id": 40,
+    "title": "Buffalo - Short Rib Fresh",
+    "price": 2.26,
+    "quantity": 98,
+    "category": "food"
+  }, {
+    "id": 41,
+    "title": "Glaze - Clear",
+    "price": 50.71,
+    "quantity": 8,
+    "category": "personal care"
+  }, {
+    "id": 42,
+    "title": "Potatoes - Fingerling 4 Oz",
+    "price": 91.55,
+    "quantity": 5,
+    "category": "beverages"
+  }, {
+    "id": 43,
+    "title": "Beer - Sleemans Honey Brown",
+    "price": 4.8,
+    "quantity": 50,
+    "category": "beverages"
+  }, {
+    "id": 44,
+    "title": "Tomatoes - Heirloom",
+    "price": 75.51,
+    "quantity": 26,
+    "category": "beverages"
+  }, {
+    "id": 45,
+    "title": "Snapple Raspberry Tea",
+    "price": 30.1,
+    "quantity": 9,
+    "category": "personal care"
+  }, {
+    "id": 46,
+    "title": "Tomatoes",
+    "price": 22.83,
+    "quantity": 92,
+    "category": "household"
+  }, {
+    "id": 47,
+    "title": "Stock - Chicken, White",
+    "price": 78.79,
+    "quantity": 75,
+    "category": "personal care"
+  }, {
+    "id": 48,
+    "title": "Bread - Roll, Soft White Round",
+    "price": 97.41,
+    "quantity": 6,
+    "category": "personal care"
+  }, {
+    "id": 49,
+    "title": "Wine - Soave Folonari",
+    "price": 67.04,
+    "quantity": 13,
+    "category": "food"
+  }, {
+    "id": 50,
+    "title": "Bread - Petit Baguette",
+    "price": 35.76,
+    "quantity": 52,
+    "category": "household"
+  }, {
+    "id": 51,
+    "title": "Peas Snow",
+    "price": 35.84,
+    "quantity": 19,
+    "category": "food"
+  }, {
+    "id": 52,
+    "title": "Rye Special Old",
+    "price": 80.08,
+    "quantity": 52,
+    "category": "household"
+  }, {
+    "id": 53,
+    "title": "Kiwi",
+    "price": 65.6,
+    "quantity": 7,
+    "category": "household"
+  }, {
+    "id": 54,
+    "title": "Kahlua",
+    "price": 77.93,
+    "quantity": 76,
+    "category": "personal care"
+  }, {
+    "id": 55,
+    "title": "Fish - Bones",
+    "price": 50.09,
+    "quantity": 10,
+    "category": "food"
+  }, {
+    "id": 56,
+    "title": "Tea - Vanilla Chai",
+    "price": 17.4,
+    "quantity": 76,
+    "category": "personal care"
+  }, {
+    "id": 57,
+    "title": "Bread Ww Cluster",
+    "price": 25.37,
+    "quantity": 31,
+    "category": "household"
+  }, {
+    "id": 58,
+    "title": "Beans - Fava Fresh",
+    "price": 15.48,
+    "quantity": 32,
+    "category": "household"
+  }, {
+    "id": 59,
+    "title": "Longos - Greek Salad",
+    "price": 48.17,
+    "quantity": 77,
+    "category": "household"
+  }, {
+    "id": 60,
+    "title": "Bandage - Fexible 1x3",
+    "price": 91.98,
+    "quantity": 45,
+    "category": "beverages"
+  }, {
+    "id": 61,
+    "title": "Octopus",
+    "price": 70.98,
+    "quantity": 6,
+    "category": "food"
+  }, {
+    "id": 62,
+    "title": "Beer - Labatt Blue",
+    "price": 57.58,
+    "quantity": 9,
+    "category": "personal care"
+  }, {
+    "id": 63,
+    "title": "Beef - Tenderloin Tails",
+    "price": 12.85,
+    "quantity": 25,
+    "category": "household"
+  }, {
+    "id": 64,
+    "title": "Veal Inside - Provimi",
+    "price": 38.98,
+    "quantity": 11,
+    "category": "household"
+  }, {
+    "id": 65,
+    "title": "Pepper - Red Chili",
+    "price": 5.92,
+    "quantity": 76,
+    "category": "beverages"
+  }, {
+    "id": 66,
+    "title": "Basil - Primerba, Paste",
+    "price": 99.22,
+    "quantity": 14,
+    "category": "personal care"
+  }, {
+    "id": 67,
+    "title": "Tea Leaves - Oolong",
+    "price": 49.21,
+    "quantity": 13,
+    "category": "household"
+  }, {
+    "id": 68,
+    "title": "Seabream Whole Farmed",
+    "price": 55.6,
+    "quantity": 8,
+    "category": "household"
+  }, {
+    "id": 69,
+    "title": "Carrots - Mini, Stem On",
+    "price": 7.73,
+    "quantity": 50,
+    "category": "personal care"
+  }, {
+    "id": 70,
+    "title": "Peach - Halves",
+    "price": 84.69,
+    "quantity": 88,
+    "category": "beverages"
+  }, {
+    "id": 71,
+    "title": "Wine - Pinot Noir Stoneleigh",
+    "price": 31.5,
+    "quantity": 84,
+    "category": "beverages"
+  }, {
+    "id": 72,
+    "title": "Bread - Ciabatta Buns",
+    "price": 60.74,
+    "quantity": 20,
+    "category": "beverages"
+  }, {
+    "id": 73,
+    "title": "Wine - Port Late Bottled Vintage",
+    "price": 15.77,
+    "quantity": 98,
+    "category": "food"
+  }, {
+    "id": 74,
+    "title": "Eel Fresh",
+    "price": 64.28,
+    "quantity": 63,
+    "category": "household"
+  }, {
+    "id": 75,
+    "title": "Pork - Back Ribs",
+    "price": 87.19,
+    "quantity": 35,
+    "category": "household"
+  }, {
+    "id": 76,
+    "title": "Wine - Shiraz Wolf Blass Premium",
+    "price": 16.88,
+    "quantity": 66,
+    "category": "food"
+  }, {
+    "id": 77,
+    "title": "Wine - Mondavi Coastal Private",
+    "price": 53.6,
+    "quantity": 39,
+    "category": "household"
+  }, {
+    "id": 78,
+    "title": "Turnip - White",
+    "price": 57.68,
+    "quantity": 78,
+    "category": "household"
+  }, {
+    "id": 79,
+    "title": "Bar Mix - Lime",
+    "price": 8.61,
+    "quantity": 28,
+    "category": "food"
+  }, {
+    "id": 80,
+    "title": "Beans - Fava, Canned",
+    "price": 57.64,
+    "quantity": 55,
+    "category": "beverages"
+  }, {
+    "id": 81,
+    "title": "Bacardi Breezer - Strawberry",
+    "price": 28.98,
+    "quantity": 91,
+    "category": "food"
+  }, {
+    "id": 82,
+    "title": "Cognac - Courvaisier",
+    "price": 82.07,
+    "quantity": 27,
+    "category": "household"
+  }, {
+    "id": 83,
+    "title": "Lid Tray - 16in Dome",
+    "price": 70.65,
+    "quantity": 69,
+    "category": "beverages"
+  }, {
+    "id": 84,
+    "title": "Mustard - Individual Pkg",
+    "price": 30.37,
+    "quantity": 60,
+    "category": "food"
+  }, {
+    "id": 85,
+    "title": "Bacon Strip Precooked",
+    "price": 18.06,
+    "quantity": 9,
+    "category": "food"
+  }, {
+    "id": 86,
+    "title": "Chicken - Base",
+    "price": 35.38,
+    "quantity": 82,
+    "category": "personal care"
+  }, {
+    "id": 87,
+    "title": "Energy Drink - Franks Pineapple",
+    "price": 52.34,
+    "quantity": 36,
+    "category": "food"
+  }, {
+    "id": 88,
+    "title": "Flour - Masa De Harina Mexican",
+    "price": 14.17,
+    "quantity": 69,
+    "category": "personal care"
+  }, {
+    "id": 89,
+    "title": "The Pop Shoppe - Root Beer",
+    "price": 14.33,
+    "quantity": 44,
+    "category": "beverages"
+  }, {
+    "id": 90,
+    "title": "Wine - Chianti Classico Riserva",
+    "price": 26.91,
+    "quantity": 6,
+    "category": "food"
+  }, {
+    "id": 91,
+    "title": "Cookie Chocolate Chip With",
+    "price": 76.01,
+    "quantity": 6,
+    "category": "household"
+  }, {
+    "id": 92,
+    "title": "Contreau",
+    "price": 87.41,
+    "quantity": 19,
+    "category": "household"
+  }, {
+    "id": 93,
+    "title": "Beef - Ground Medium",
+    "price": 72.1,
+    "quantity": 86,
+    "category": "personal care"
+  }, {
+    "id": 94,
+    "title": "Chinese Foods - Plain Fried Rice",
+    "price": 29.19,
+    "quantity": 9,
+    "category": "food"
+  }, {
+    "id": 95,
+    "title": "Table Cloth 62x120 White",
+    "price": 50.17,
+    "quantity": 1,
+    "category": "beverages"
+  }, {
+    "id": 96,
+    "title": "Crab - Claws, 26 - 30",
+    "price": 80.21,
+    "quantity": 85,
+    "category": "household"
+  }, {
+    "id": 97,
+    "title": "Jolt Cola",
+    "price": 7.22,
+    "quantity": 6,
+    "category": "beverages"
+  }, {
+    "id": 98,
+    "title": "Yogurt - Plain",
+    "price": 68.26,
+    "quantity": 30,
+    "category": "personal care"
+  }, {
+    "id": 99,
+    "title": "Napkin Colour",
+    "price": 78.76,
+    "quantity": 100,
+    "category": "beverages"
+  }, {
+    "id": 100,
+    "title": "Chocolate - Chips Compound",
+    "price": 60.16,
+    "quantity": 73,
+    "category": "beverages"
+  }]
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { products } from "../data/productData";
+import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from "react-icons/md";
 
 const Products = () => {
   const [data, setData] = useState(products);
   const [order, setOrder] = useState("ASC");
+  const [search, setSearch] = useState("");
 
   const sorting = (col) => {
     if (order === "ASC") {
@@ -41,49 +43,87 @@ const Products = () => {
       </div>
       <div className="w-full">
         <div className="overflow-x-auto">
-          <table className="table my-8">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Search product"
+            className="input input-bordered w-full max-w-xs my-5 mx-2 float-right"
+          />
+          <table className="table my-8 w-full">
             {/* head */}
-            <thead className="bg-slate-200 text-[16px] text-slate-800">
+            <thead className="bg-slate-200 text-[16px] text-slate-800 w-full">
               <tr>
                 <th
-                  className="cursor-pointer"
+                  className="cursor-pointer flex items-center"
                   onClick={() => sorByNumber("id")}
                 >
-                  No
+                  <p>No</p>
+                  {order === "ASC" ? (
+                    <MdOutlineArrowDropDown />
+                  ) : (
+                    <MdOutlineArrowDropUp />
+                  )}
                 </th>
+
                 <th className="cursor-pointer" onClick={() => sorting("title")}>
                   Name
+                  {order === "ASC" ? (
+                    <MdOutlineArrowDropDown />
+                  ) : (
+                    <MdOutlineArrowDropUp />
+                  )}
                 </th>
                 <th
                   className="cursor-pointer"
                   onClick={() => sorting("category")}
                 >
                   Category
+                  {order === "ASC" ? (
+                    <MdOutlineArrowDropDown />
+                  ) : (
+                    <MdOutlineArrowDropUp />
+                  )}
                 </th>
                 <th
-                  className="cursor-pointer"
+                  className="cursor-pointer flex items-center"
                   onClick={() => sorByNumber("price")}
                 >
                   Price
+                  {order === "ASC" ? (
+                    <MdOutlineArrowDropDown />
+                  ) : (
+                    <MdOutlineArrowDropUp />
+                  )}
                 </th>
                 <th
                   className="cursor-pointer"
                   onClick={() => sorByNumber("quantity")}
                 >
                   Quantity
+                  {order === "ASC" ? (
+                    <MdOutlineArrowDropDown />
+                  ) : (
+                    <MdOutlineArrowDropUp />
+                  )}
                 </th>
               </tr>
             </thead>
             <tbody>
-              {data.map(({ id, title, category, price, quantity }) => (
-                <tr key={id} className="border-b">
-                  <td className="">{id}</td>
-                  <td className="">{title}</td>
-                  <td>{category}</td>
-                  <td>${price}</td>
-                  <td>{quantity ? quantity : "Out of stock"}</td>
-                </tr>
-              ))}
+              {data
+                .filter((item) => {
+                  return search.toLowerCase() === ""
+                    ? item
+                    : item.title.toLowerCase().includes(search);
+                })
+                .map(({ id, title, category, price, quantity }) => (
+                  <tr key={id} className="border-b">
+                    <td className="">{id}</td>
+                    <td className="">{title}</td>
+                    <td>{category}</td>
+                    <td>${price}</td>
+                    <td>{quantity ? quantity : "Out of stock"}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
